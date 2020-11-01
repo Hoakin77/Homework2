a38bfad713610aaa1bde0f5eb08f68b09fb91191
Munir <munirhasanov13@gmail.com>
2020-11-01 18:31:29 +0300

    Initial commit

---
+++ src/com/mkhasanovcorparation/Main.java
@@ -0,0 +1,22 @@
+package com.mkhasanovcorparation;
+
+public class Main {
+
+    public static void main(String[] args) { //задание 3
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] < 6) arr3[i] *= 2;
+            System.out.print(arr3[i] + " ");
+        }
+        //задание 4
+        int[][] arr4 = new int[7][7];
+        for (int i = 0; i < arr4.length; i++) {
+            for (int j = 0, j2 = arr4[i].length; j < arr4[i].length; j++, j2--) {
+                if (i == j || i == (j2 - 1)) arr4[i][j] = 1;
+                System.out.print(arr4[i][j] + " ");
+            }
+            System.out.print("\r\n");
+        }
+
+    }
+}
