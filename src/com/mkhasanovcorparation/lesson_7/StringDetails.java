5d2d854b6c0065afb2925aed6e316717ff44cdb9
Munir <munirhasanov13@gmail.com>
2020-11-20 17:20:34 +0300

    homework7

---
+++ src/com/mkhasanovcorparation/lesson_7/StringDetails.java
@@ -0,0 +1,16 @@
+package com.mkhasanovcorparation.lesson_7;
+
+import java.nio.charset.StandardCharsets;
+
+public class StringDetails {
+    public static void main(String[] args) {
+       /* String s1 = "Java";
+        String s2 = new String("Home");
+        char [] value = {'A', 'B', 'C'};
+        String s3 = new String(value);
+        String s4 = new String(s3);
+        String s5 = new String(new byte[]{65, 66, 67});
+        String s6 = new String(new byte[]{0, 65, 0, 66}, StandardCharsets.UTF_16);
+        System.out.printf("s1 = %s, s2 = %s, s3 = %s, s4 = %s, s5 = %s, s6 = %s", s1, s2, s3, s4, s5, s6);*/
+    }
+}
